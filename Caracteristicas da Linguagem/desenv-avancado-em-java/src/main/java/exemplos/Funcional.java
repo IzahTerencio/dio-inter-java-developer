85e6c68cb76371447b41d8937e341a31c71b1f85
Izabella Terencio <izahterencio@gmail.com>
2021-03-22 18:30:35 -0300

    Exemplo com Paradigma Funcional

---
+++ Caracteristicas da Linguagem/desenv-avancado-em-java/src/main/java/exemplos/Funcional.java
@@ -0,0 +1,14 @@
+package exemplos;
+
+import java.util.function.UnaryOperator;
+
+public class Funcional {
+
+    public static void main(String[] args){
+
+        UnaryOperator <Integer> calcularValorVezesTres = valor -> valor*3;
+        var valor = 10;
+
+    }
+
+}
